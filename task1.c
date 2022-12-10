bbe12f706e2ecf5638bc3ee54f7c8c24b0f26a93
artem_herashchenko <artem.herashchenko@gmail.com>
2022-12-10 11:23:15 +0200

    completed 1st task

---
+++ task1.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+
+int main(){
+    printf("Hello world!)\n"
+            "My group number: 1\n"
+            "Full name: Artem Herashchenko\n"
+            "Email: artem.herashchenko@gmail.com\n"
+            "GitHub: https: //github.com/ArtemHerashchenko/GL_Embedded_BaseCamp\n"
+            "Google drive: https: //drive.google.com/drive/folders/1tCodhZX6x3GWlxpxqDEuS5dcWDJwaAxp?usp=sharing\n");
+    
+    return 0;
+}
